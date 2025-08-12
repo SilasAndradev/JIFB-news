cfa6a7edbe9b4befc21be0b99ab13c453bd8354c
Silas <silasandrade.code@gmail.com>
2025-08-12 20:44:44 -0300

    fix: adicionar configuração de ssl_require e comentário sobre conn_max_age no banco de dados fix: criar migração para alterar campo foto_de_perfil no modelo perfil fix: criar migração para alterar campo autor no modelo noticia

--- JIFB/settings.py
+++ JIFB/settings.py
@@ -101,7 +101,8 @@ DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
         default=os.getenv("DATABASE_URL"),
-        conn_max_age=600
+        conn_max_age=600,  # mantém conexão aberta para performance
+        ssl_require=True 
     )
 }
 
