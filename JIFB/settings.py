196b7776c8242ec1098b9e2c4b5b67327471959c
Silas <silasandrade.code@gmail.com>
2025-08-12 20:21:45 -0300

    fix: adicionar suporte ao dj_database_url para configuração do banco de dados

--- JIFB/settings.py
+++ JIFB/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
-
+import dj_database_url
 load_dotenv() # Carrega as variáveis de ambiente do .env
 
 
@@ -98,10 +98,11 @@ WSGI_APPLICATION = 'JIFB.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600
+    )
 }
 
 
